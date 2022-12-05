edda3ee9fbb6831038660bbf91bf17fb10fd99e4
Are-Jae <evansraven7@gmail.com>
2022-12-05 16:42:37 -0500

    added js, code not working

---
+++ script.js
@@ -0,0 +1,18 @@
+/*var bioText = document.getElementById ("#text-block"); /*I want to have this load after user clicks "Click Here", trigger event? but which event?*/
+
+/*generateBtn.addEventListener ("click", function() { /*have button & eventListener but not sure of what to do next, do I need button since I'm using href in html?*/ 
+
+/*var textImg = bioText();
+} )
+
+if ( textImg= bioText ) {
+    display /*what function should I use?
+};*/
+
+let img = document.getElementById("#text-block");
+
+img.src="assets/dialogue box ss.JPG";
+
+img.addEventListener("click", function() {
+    console.log("clicked");
+  })document.body.appendChild(img);
