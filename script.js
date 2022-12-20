d2bfb4c7e7304967690583a4ecca06a11aa312da
Are-Jae <evansraven7@gmail.com>
2022-12-19 21:22:29 -0500

    added notes, structure, linked new html

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-/*let bioButton= "profile";
-let profImg= "clicked"
-;
+const bioButton = document.getElementById("profile")
+const bioImg = document.querySelector(".container");
+const textBlock = document.querySelector(".text-block")
 
- document.getElementById("profile").addEventListener("click", function show_image()){
-    if (bioButton = "clicked" ) {   img.src="assets/dialogue box ss.JPG"
- }};
-    /*else{
-        don't display (cannot recall how to use js logic for this)
-    };
+bioImg.style.opacity= "0";
+textBlock.style.opacity= "0";
+
+// add event listener to  bio button so that when we click it, something will hapenn
+bioButton.addEventListener("click", function () {
+    bioImg.style.opacity = "1"
+    bioImg.style.transitionDelay = '.5s'
+    textBlock.style.opacity = "1"
+    textBlock.style.transitionDelay = ".75s"
+})
 
 
-/*img.src="assets/dialogue box ss.JPG";
 
-img.addEventListener("click", function() {
-    console.log("clicked");
-  });document.body.appendChild(img);*/
+ 
 
- /* <img src="assets/pfpdemo.jpg" onclick="window.open(this.src)"></img> is supposed to open pfp in new window, does not. not really what I'm trying to do */
-
- let bioButton= document.getElementById("profile").addEventListener;
- let profImg = new Image;
- newImage.src="assets/dialogue box ss.JPG";
- bioButton.addEventListener("click", () => {
-    profImg.appendChild(newImage)});
-
-/*do I need to apply a math function?*/
